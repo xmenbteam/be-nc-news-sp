321665e11a323ccc207c3f57a0dccff6e26be611
Sam Parry <samparrymusic@gmail.com>
2020-11-30 15:43:57 +0000

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -244,24 +244,3 @@ describe('/api', () => {
         })
     })
 })
-
-/*
-PATCH / api / comments /: comment_id
-Request body accepts
-an object in the form { inc_votes: newVote }
-
-newVote will indicate how much the votes property in the database should be updated by
-e.g.
-
-{ inc_votes: 1 } would increment the current comment's vote property by 1
-
-{ inc_votes: -1 } would decrement the current comment's vote property by 1
-
-Responds with
-the updated comment
-DELETE / api / comments /: comment_id
-Should
-delete the given comment by comment_id
-Responds with
-status 204 and no content
-*/
